432c23e85c977c5e430188da5742337aa30e8823
Andres Rengifo <andres.rengifo@bold.co>
2021-03-22 22:24:57 -0500

    style: some checks about semicolons

--- api/src/mutants/mutants.service.ts
+++ api/src/mutants/mutants.service.ts
@@ -13,7 +13,7 @@ export class MutantsDnaVerification {
     let verificationResult = [];
     each(dna, (item, i) => {
       verificationResult[i] = new RegExp(_MATCHES.join("|")).test(item);
-    });
+    })
     return verificationResult;
   }
 
@@ -21,22 +21,20 @@ export class MutantsDnaVerification {
     let joinedDna = []
     each(dna, (item, i) => {
       joinedDna[i] = item.join('')
-    });
-
+    })
     return joinedDna;
   }
 
   checkDnaRows(dna: Array<string>): boolean {
     return includes(this.verifyDna(dna), true);
-  };
+  }
 
   createEmptyDnaSequence(dnaSequenceLength: number): Array<Array<string>> {
     let emptyDnaSequence = []
     for (let i = 0; i < dnaSequenceLength; i++) {
       emptyDnaSequence.push([]);
-    };
-
-    return emptyDnaSequence
+    }
+    return emptyDnaSequence;
   }
 
   checkDnaDiagonals(dna: Array<string>): boolean {
@@ -47,8 +45,7 @@ export class MutantsDnaVerification {
       for (let j = 0; j < dnaLength; j++) {
         dnaDiagonals[i + j].push(dna[j][i]);
       }
-    };
-
+    }
     return includes(this.verifyDna(this.joinDnaSequence(dnaDiagonals)), true);
   }
 
